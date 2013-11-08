97b1040b0c742e590c8a5f3497e6bc094a0fdcb1
RezaOlix <reza@onmego.com>
2013-11-07 16:48:32 -0800

    initial blog commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QBlog::Application.config.secret_key_base = 'a5bcd0e7dc9a3388746a7518da9eea4ca35a6d77faa7987055dd82fa9c7af55d2352373c0f7f416dc03944fe87b7da85d34b0fbb0914c149dec56b85e62005e7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+QBlog::Application.config.session_store :cookie_store, key: '_q_blog_session'
